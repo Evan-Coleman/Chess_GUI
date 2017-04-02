86a1e72cad9b896d34dc969581faff3709269690
Evan Coleman <ecoleman2@gmail.com>
2017-04-01 20:02:12 -0700

    Implemented ICommand interface and made the viewmodel's board an observable collection (not sure if I need it). Wired up one button to execute a dummy command on click.

--- Chess_GUI/ViewModels/BoardViewModel.cs
+++ Chess_GUI/ViewModels/BoardViewModel.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Documents;
 using Chess_GUI.Models;
+using Chess_GUI.ViewModels.Commands;
 
 namespace Chess_GUI.ViewModels
 {
@@ -14,6 +18,10 @@ namespace Chess_GUI.ViewModels
         {
             Board = new Board();
 
+            MyBoard = new ObservableCollection<Board>()
+            {
+                Board
+            };
 
             var spaces = new List<char>();
             Board.MBoard = new List<List<char>>();
@@ -23,7 +31,7 @@ namespace Chess_GUI.ViewModels
                 spaces.Add(' ');
             }
 
-            for (int i = 0; i < 8; i++)
+            for (var i = 0; i < 8; i++)
             {       // sets board to all zeros in order to show an empty board
                 Board.MBoard.Add(spaces);
                 //spaces.Clear();
@@ -43,35 +51,48 @@ namespace Chess_GUI.ViewModels
 
             }
 
+            MoveCommand = new RelayCommand(Move, Canexecute);
+
         }
+
+        public ObservableCollection<Board> MyBoard { get; private set; }
+
+        public RelayCommand MoveCommand { get; private set; }
 
         public Board Board { get; set; }
 
-        //public BoardViewModel BoardViewModel { get; set; }
-
-
-
-        public void poke()
+        public void Move(object message)
         {
-            Board.MBoard[0][0] = 'a';
+            MessageBox.Show("HELLO!");
         }
 
-        // Checks to see if the input is formatted correctly
-        public int ValidInputCheck(string opt)
+        public bool Canexecute(object message)
         {
+            return true;
+        }
+        // Checks to see if the input is formatted correctly
+        public bool ValidInputCheck(object message)
+        {
+
+            if (message == null)
+            {
+                return false;
+            }
+            var opt = (string)message;
             opt = opt.ToLower();
 
+            if (opt.Length != 4) // a valid move will always be 4 chars
+                return false;
+
             if (opt[1] == opt[3] && opt[2] == opt[4])   // can't move to the same spot
-                return 0;
+                return false;
 
             const string m = "m";
             const string nums = "12345678";
             const string lets = "abcdefgh";
             var j = 0;
-            if (opt.Length != 5) // a valid move will always be 5 chars
-                return 0;
             if (opt[0] != m[0])
-                return 0;
+                return false;
             for (var i = 0; i < 8; i++)
             {
                 if (opt[1] == lets[i])
@@ -83,7 +104,7 @@ namespace Chess_GUI.ViewModels
                 if (opt[4] == nums[i])
                     j++;
             }
-            return j != 4 ? 0 : 1;
+            return j == 4;
         }
 
     }
